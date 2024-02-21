ad76b40198114da614ee7d950a679686b62c3856
Priyam Parashar <priyam8parashar@gmail.com>
2024-02-21 23:47:51 +0000

    formatting

--- conceptgraph/utils/vis.py
+++ conceptgraph/utils/vis.py
@@ -13,15 +13,17 @@ import supervision as sv
 from supervision.draw.color import Color, ColorPalette
 from conceptgraph.slam.slam_classes import MapObjectList
 
-class OnlineObjectRenderer():
-    '''
+
+class OnlineObjectRenderer:
+    """
     Refactor of the open3d visualization code to make it more modular
-    '''
+    """
+
     def __init__(
-        self, 
+        self,
         view_param: str | dict,
         base_objects: MapObjectList | None = None,
-        gray_map: bool = False
+        gray_map: bool = False,
     ) -> None:
         # If the base objects are provided, we will visualize them
         if base_objects is not None:
@@ -35,40 +37,42 @@ class OnlineObjectRenderer():
                     base_pcds_vis[i].paint_uniform_color([0.5, 0.5, 0.5])
             for i in range(self.n_base_objects):
                 base_bboxes_vis[i].color = [0.5, 0.5, 0.5]
-            
+
             self.base_pcds_vis = base_pcds_vis
             self.base_bboxes_vis = base_bboxes_vis
         else:
             self.n_base_objects = 0
-        
+
         self.est_traj = []
         self.gt_traj = []
-        
+
         self.cmap = matplotlib.colormaps.get_cmap("turbo")
 
         if isinstance(view_param, str):
             self.view_param = o3d.io.read_pinhole_camera_parameters(view_param)
         else:
             self.view_param = view_param
-            
+
         self.window_height = self.view_param.intrinsic.height
         self.window_width = self.view_param.intrinsic.width
-        
+
         self.vis = o3d.visualization.Visualizer()
         self.vis.create_window(
-            width = self.window_width,
-            height = self.window_height,
+            width=self.window_width,
+            height=self.window_height,
         )
-        
+
         self.vis_ctrl = self.vis.get_view_control()
         self.vis_ctrl.convert_from_pinhole_camera_parameters(self.view_param)
-        
+
     def filter_base_by_mask(self, mask: Iterable[bool]):
         assert len(mask) == self.n_base_objects
         self.base_pcds_vis = [pcd for pcd, m in zip(self.base_pcds_vis, mask) if m]
-        self.base_bboxes_vis = [bbox for bbox, m in zip(self.base_bboxes_vis, mask) if m]
+        self.base_bboxes_vis = [
+            bbox for bbox, m in zip(self.base_bboxes_vis, mask) if m
+        ]
         self.n_base_objects = len(self.base_pcds_vis)
-    
+
     def step(
         self,
         image: Image.Image,
@@ -83,28 +87,32 @@ class OnlineObjectRenderer():
     ):
         # Remove all the geometries
         self.vis.clear_geometries()
-        
+
         # Add the pose cameras and trajectories
         if est_pose is not None:
             self.est_traj.append(est_pose)
             est_camera_frustum = better_camera_frustum(
-                est_pose, image.height, image.width, scale=0.5, color=[1., 0, 0]
+                est_pose, image.height, image.width, scale=0.5, color=[1.0, 0, 0]
             )
             self.vis.add_geometry(est_camera_frustum)
             if len(self.est_traj) > 1:
-                est_traj_lineset = poses2lineset(np.stack(self.est_traj), color=[1., 0, 0])
+                est_traj_lineset = poses2lineset(
+                    np.stack(self.est_traj), color=[1.0, 0, 0]
+                )
                 self.vis.add_geometry(est_traj_lineset)
-            
+
         if gt_pose is not None:
             self.gt_traj.append(gt_pose)
             gt_camera_frustum = better_camera_frustum(
-                gt_pose, image.height, image.width, scale=0.5, color=[0, 1., 0]
+                gt_pose, image.height, image.width, scale=0.5, color=[0, 1.0, 0]
             )
             self.vis.add_geometry(gt_camera_frustum)
             if len(self.gt_traj) > 1:
-                gt_traj_lineset = poses2lineset(np.stack(self.gt_traj), color=[0, 1., 0])
+                gt_traj_lineset = poses2lineset(
+                    np.stack(self.gt_traj), color=[0, 1.0, 0]
+                )
                 self.vis.add_geometry(gt_traj_lineset)
-    
+
         # Add the base objects
         if self.n_base_objects > 0:
             if base_objects_color is not None:
@@ -112,10 +120,10 @@ class OnlineObjectRenderer():
                     color = base_objects_color[obj_id]
                     self.base_pcds_vis[obj_id].paint_uniform_color(color)
                     self.base_bboxes_vis[obj_id].color = color
-            
+
             for geom in self.base_pcds_vis + self.base_bboxes_vis:
                 self.vis.add_geometry(geom)
-            
+
         # Show the extra pcds to visualize
         if pcds is not None:
             for i in range(len(pcds)):
@@ -123,116 +131,142 @@ class OnlineObjectRenderer():
                 if pcd_colors is not None:
                     pcds[i].paint_uniform_color(pcd_colors[i][:3])
                 self.vis.add_geometry(pcds[i])
-            
+
         # Show the extra new objects
         if new_objects is not None:
             for obj in new_objects:
-                pcd = copy.deepcopy(obj['pcd'])
-                bbox = copy.deepcopy(obj['bbox'])
+                pcd = copy.deepcopy(obj["pcd"])
+                bbox = copy.deepcopy(obj["bbox"])
                 bbox.color = [0.0, 0.0, 1.0]
                 if paint_new_objects:
                     pcd.paint_uniform_color([0.0, 1.0, 0.0])
                     bbox.color = [0.0, 1.0, 0.0]
-                
+
                 self.vis.add_geometry(pcd)
                 self.vis.add_geometry(bbox)
-        
+
         self.vis_ctrl.convert_from_pinhole_camera_parameters(self.view_param)
-        
+
         self.vis.poll_events()
         self.vis.update_renderer()
-        
+
         rendered_image = self.vis.capture_screen_float_buffer(False)
         rendered_image = np.asarray(rendered_image)
-        
+
         if return_vis_handle:
             return rendered_image, self.vis
         else:
             return rendered_image, None
 
+
 def get_random_colors(num_colors):
-    '''
+    """
     Generate random colors for visualization
-    
+
     Args:
         num_colors (int): number of colors to generate
-        
+
     Returns:
         colors (np.ndarray): (num_colors, 3) array of colors, in RGB, [0, 1]
-    '''
+    """
     colors = []
     for i in range(num_colors):
         colors.append(np.random.rand(3))
     colors = np.array(colors)
     return colors
 
+
 def show_mask(mask, ax, random_color=False):
     if random_color:
         color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
     else:
-        color = np.array([30/255, 144/255, 255/255, 0.6])
+        color = np.array([30 / 255, 144 / 255, 255 / 255, 0.6])
     h, w = mask.shape[-2:]
     mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
     ax.imshow(mask_image)
-    
+
+
 def show_points(coords, labels, ax, marker_size=375):
-    pos_points = coords[labels==1]
-    neg_points = coords[labels==0]
-    ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
-    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)   
-    
+    pos_points = coords[labels == 1]
+    neg_points = coords[labels == 0]
+    ax.scatter(
+        pos_points[:, 0],
+        pos_points[:, 1],
+        color="green",
+        marker="*",
+        s=marker_size,
+        edgecolor="white",
+        linewidth=1.25,
+    )
+    ax.scatter(
+        neg_points[:, 0],
+        neg_points[:, 1],
+        color="red",
+        marker="*",
+        s=marker_size,
+        edgecolor="white",
+        linewidth=1.25,
+    )
+
+
 def show_box(box, ax, label=None):
     x0, y0 = box[0], box[1]
     w, h = box[2] - box[0], box[3] - box[1]
-    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2))    
-    
+    ax.add_patch(
+        plt.Rectangle((x0, y0), w, h, edgecolor="green", facecolor=(0, 0, 0, 0), lw=2)
+    )
+
     if label is not None:
         ax.text(x0, y0, label)
-        
+
+
 def vis_result_fast(
-    image: np.ndarray, 
-    detections: sv.Detections, 
-    classes: list[str], 
-    color: Color | ColorPalette = ColorPalette.default(), 
+    image: np.ndarray,
+    detections: sv.Detections,
+    classes: list[str],
+    color: Color | ColorPalette = ColorPalette.default(),
     instance_random_color: bool = False,
     draw_bbox: bool = True,
 ) -> np.ndarray:
-    '''
-    Annotate the image with the detection results. 
-    This is fast but of the same resolution of the input image, thus can be blurry. 
-    '''
+    """
+    Annotate the image with the detection results.
+    This is fast but of the same resolution of the input image, thus can be blurry.
+    """
     # annotate image with detections
     box_annotator = sv.BoxAnnotator(
-        color = color,
+        color=color,
         text_scale=0.3,
         text_thickness=1,
         text_padding=2,
     )
-    mask_annotator = sv.MaskAnnotator(
-        color = color
-    )
+    mask_annotator = sv.MaskAnnotator(color=color)
     labels = [
-        f"{classes[class_id]} {confidence:0.2f}" 
-        for _, _, confidence, class_id, _ 
-        in detections]
-    
+        f"{classes[class_id]} {confidence:0.2f}"
+        for _, _, confidence, class_id, _, _ in detections
+    ]
+
     if instance_random_color:
         # generate random colors for each segmentation
         # First create a shallow copy of the input detections
         detections = dataclasses.replace(detections)
         detections.class_id = np.arange(len(detections))
-        
+
     annotated_image = mask_annotator.annotate(scene=image.copy(), detections=detections)
-    
+
     if draw_bbox:
-        annotated_image = box_annotator.annotate(scene=annotated_image, detections=detections, labels=labels)
+        annotated_image = box_annotator.annotate(
+            scene=annotated_image, detections=detections, labels=labels
+        )
     return annotated_image, labels
 
-def vis_result_slow_caption(image, masks, boxes_filt, pred_phrases, caption, text_prompt):
-    '''
+
+def vis_result_slow_caption(
+    image, masks, boxes_filt, pred_phrases, caption, text_prompt
+):
+    """
     Annotate the image with detection results, together with captions and text prompts.
     This function is very slow, but the output is more readable.
-    '''
+    """
     plt.figure(figsize=(10, 10))
     plt.imshow(image)
     for mask in masks:
@@ -240,9 +274,11 @@ def vis_result_slow_caption(image, masks, boxes_filt, pred_phrases, caption, tex
     for box, label in zip(boxes_filt, pred_phrases):
         show_box(box, plt.gca(), label)
 
-    plt.title('Tagging-Caption: ' + caption + '\n' + 'Tagging-classes: ' + text_prompt + '\n')
-    plt.axis('off')
-    
+    plt.title(
+        "Tagging-Caption: " + caption + "\n" + "Tagging-classes: " + text_prompt + "\n"
+    )
+    plt.axis("off")
+
     # Convert the fig to a numpy array
     fig = plt.gcf()
     fig.tight_layout(pad=0)
@@ -250,36 +286,47 @@ def vis_result_slow_caption(image, masks, boxes_filt, pred_phrases, caption, tex
     vis_image = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
     vis_image = vis_image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
     plt.close()
-    
+
     return vis_image
 
-def vis_sam_mask(anns):
-    sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
 
-    img = np.ones((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1], 4))
-    img[:,:,3] = 0
+def vis_sam_mask(anns):
+    sorted_anns = sorted(anns, key=(lambda x: x["area"]), reverse=True)
+
+    img = np.ones(
+        (
+            sorted_anns[0]["segmentation"].shape[0],
+            sorted_anns[0]["segmentation"].shape[1],
+            4,
+        )
+    )
+    img[:, :, 3] = 0
     for ann in sorted_anns:
-        m = ann['segmentation']
+        m = ann["segmentation"]
         color_mask = np.concatenate([np.random.random(3), [0.35]])
         img[m] = color_mask
-        
+
     return img
 
+
 def poses2lineset(poses, color=[0, 0, 1]):
-    '''
+    """
     Create a open3d line set from a batch of poses
 
     poses: (N, 4, 4)
     color: (3,)
-    '''
+    """
     N = poses.shape[0]
     lineset = o3d.geometry.LineSet()
     lineset.points = o3d.utility.Vector3dVector(poses[:, :3, 3])
     lineset.lines = o3d.utility.Vector2iVector(
         np.array([[i, i + 1] for i in range(N - 1)])
     )
-    lineset.colors = o3d.utility.Vector3dVector([color for _ in range(len(lineset.lines))])
+    lineset.colors = o3d.utility.Vector3dVector(
+        [color for _ in range(len(lineset.lines))]
+    )
     return lineset
+
 
 def create_camera_frustum(
     camera_pose, width=1, height=1, z_near=0.5, z_far=1, color=[0, 0, 1]
@@ -308,15 +355,17 @@ def better_camera_frustum(camera_pose, img_h, img_w, scale=3.0, color=[0, 0, 1])
     # Convert camera pose tensor to numpy array
     if isinstance(camera_pose, torch.Tensor):
         camera_pose = camera_pose.numpy()
-    
+
     # Define near and far distance (adjust these as needed)
     near = scale * 0.1
     far = scale * 1.0
-    
+
     # Define frustum dimensions at the near plane (replace with appropriate values)
     frustum_h = near
-    frustum_w = frustum_h * img_w / img_h  # Set frustum width based on its height and the image aspect ratio
-    
+    frustum_w = (
+        frustum_h * img_w / img_h
+    )  # Set frustum width based on its height and the image aspect ratio
+
     # Compute the 8 points that define the frustum
     points = []
     for x in [-1, 1]:
@@ -324,17 +373,30 @@ def better_camera_frustum(camera_pose, img_h, img_w, scale=3.0, color=[0, 0, 1])
             for z in [-1, 1]:
                 u = x * (frustum_w // 2 if z == -1 else frustum_w * far / near)
                 v = y * (frustum_h // 2 if z == -1 else frustum_h * far / near)
-                d = near if z == -1 else far # Negate depth here
+                d = near if z == -1 else far  # Negate depth here
                 # d = -near if z == -1 else -far # Negate depth here
                 point = np.array([u, v, d, 1]).reshape(-1, 1)
                 transformed_point = (camera_pose @ point).ravel()[:3]
                 # transformed_point[0] *= -1  # Flip X-coordinate
-                points.append(transformed_point) # Using camera pose directly
+                points.append(transformed_point)  # Using camera pose directly
                 # points.append((camera_pose_np @ point).ravel()[:3]) # Using camera pose directly
-    
+
     # Create lines that connect the 8 points
-    lines = [[0, 1], [1, 3], [3, 2], [2, 0], [4, 5], [5, 7], [7, 6], [6, 4], [0, 4], [1, 5], [3, 7], [2, 6]]
-    
+    lines = [
+        [0, 1],
+        [1, 3],
+        [3, 2],
+        [2, 0],
+        [4, 5],
+        [5, 7],
+        [7, 6],
+        [6, 4],
+        [0, 4],
+        [1, 5],
+        [3, 7],
+        [2, 6],
+    ]
+
     frustum = o3d.geometry.LineSet()
     frustum.points = o3d.utility.Vector3dVector(points)
     frustum.lines = o3d.utility.Vector2iVector(lines)
@@ -377,8 +439,11 @@ class LineMesh(object):
             radius {float} -- radius of cylinder (default: {0.15})
         """
         self.points = np.array(points)
-        self.lines = np.array(
-            lines) if lines is not None else self.lines_from_ordered_points(self.points)
+        self.lines = (
+            np.array(lines)
+            if lines is not None
+            else self.lines_from_ordered_points(self.points)
+        )
         self.colors = np.array(colors)
         self.radius = radius
         self.cylinder_segments = []
@@ -407,9 +472,9 @@ class LineMesh(object):
             translation = first_points[i, :] + line_segment * line_length * 0.5
             # create cylinder and apply transformations
             cylinder_segment = o3d.geometry.TriangleMesh.create_cylinder(
-                self.radius, line_length)
-            cylinder_segment = cylinder_segment.translate(
-                translation, relative=False)
+                self.radius, line_length
+            )
+            cylinder_segment = cylinder_segment.translate(translation, relative=False)
             if axis is not None:
                 axis_a = axis * angle
                 # cylinder_segment = cylinder_segment.rotate(
@@ -417,7 +482,8 @@ class LineMesh(object):
                 # cylinder_segment = cylinder_segment.rotate(
                 #   axis_a, center=True, type=o3d.geometry.RotationType.AxisAngle)
                 cylinder_segment = cylinder_segment.rotate(
-                    R=o3d.geometry.get_rotation_matrix_from_axis_angle(axis_a))
+                    R=o3d.geometry.get_rotation_matrix_from_axis_angle(axis_a)
+                )
             # color cylinder
             color = self.colors if self.colors.ndim == 1 else self.colors[i, :]
             cylinder_segment.paint_uniform_color(color)
